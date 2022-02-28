03944cef6ebb06b084e72565b42e69da03bbfd01
ChrisIsKing <christopher.clarke@v75inc.com>
2022-02-28 01:27:04 -0500

    added eval

--- zeroshot_encoder/baseline/binary_bert.py
+++ zeroshot_encoder/baseline/binary_bert.py
@@ -2,6 +2,10 @@ import math
 import logging
 import os
 import random
+import seaborn as sns
+import pandas as pd
+from argparse import ArgumentParser
+from sklearn.metrics import classification_report
 from statistics import mode
 from torch import ge
 from torch.utils.data import DataLoader
@@ -10,29 +14,72 @@ from sentence_transformers.cross_encoder import CrossEncoder
 from sentence_transformers.cross_encoder.evaluation import CESoftmaxAccuracyEvaluator
 from dataset.load_data import get_all_zero_data, binary_cls_format
 
+def parse_args():
+    args =  ArgumentParser()
+
+    args.add_argument('--train', action='store_true', help='Train model')
+    args.add_argument('--test', action='store_true', help='Evaluate mode')
+    return args.parse_args()
+
 logger = logging.getLogger(__name__)
 
 data = get_all_zero_data()
-train = binary_cls_format(data["all"]["train"])
-test = binary_cls_format(data["all"]["test"], train=False)
 
-train_batch_size = 16
-num_epochs = 1
-model_save_path = 'models/binary_bert'
+if __name__ == "__main__":
+    args = parse_args()
+    if args.train:
+        train = binary_cls_format(data["all"]["train"])
+        test = binary_cls_format(data["all"]["test"], train=False)
 
-model = CrossEncoder('bert-base-uncased', num_labels=2)
+        train_batch_size = 16
+        num_epochs = 5
+        model_save_path = 'models/binary_bert'
 
-train_dataloader = DataLoader(train, shuffle=True, batch_size=train_batch_size)
+        model = CrossEncoder('bert-base-uncased', num_labels=2)
 
-evaluator = CESoftmaxAccuracyEvaluator.from_input_examples(test, name='UTCD-test')
+        train_dataloader = DataLoader(train, shuffle=True, batch_size=train_batch_size)
 
-warmup_steps = math.ceil(len(train_dataloader) * num_epochs * 0.1) #10% of train data for warm-up
-logger.info("Warmup-steps: {}".format(warmup_steps))
+        evaluator = CESoftmaxAccuracyEvaluator.from_input_examples(test, name='UTCD-test')
 
-# Train the model
-model.fit(train_dataloader=train_dataloader,
-          evaluator=evaluator,
-          epochs=num_epochs,
-          evaluation_steps=10000,
-          warmup_steps=warmup_steps,
-          output_path=model_save_path)
+        warmup_steps = math.ceil(len(train_dataloader) * num_epochs * 0.1) #10% of train data for warm-up
+        logger.info("Warmup-steps: {}".format(warmup_steps))
+
+        # Train the model
+        model.fit(train_dataloader=train_dataloader,
+                evaluator=evaluator,
+                epochs=num_epochs,
+                evaluation_steps=10000,
+                warmup_steps=warmup_steps,
+                output_path=model_save_path)
+    if args.test:
+        # get keys from data dict
+        datasets = list(data.keys())
+        datasets.remove("all")
+
+        # load model
+        model = CrossEncoder('./models/binary_bert')
+
+        label_map = ["true", "false"]
+
+        # loop through all datasets
+        for dataset in datasets:
+            test = data[dataset]["test"]
+            labels = list(set(x[1] for x in test))
+            gold = [x[1] for x in test]
+
+            preds = []
+            # loop through each test example
+            for example in test:
+                query = [(label, example[0]) for label in labels]
+                results = model.predict(query, apply_softmax=True)
+
+                # compute which pred is higher
+                pred = labels[results[:,0].argmax()]
+                preds.append(pred)
+            
+            report = classification_report(gold, preds, output_dict=True)
+            plt = sns.heatmap(pd.DataFrame(report).iloc[:-1, :].T, annot=True)
+            plt.figure.savefig('figures/binary_bert_{}.png'.format(dataset))
+            df = pd.DataFrame(report).transpose()
+            df.to_csv('./results/binary_bert_{}.csv'.format(dataset))
+            
