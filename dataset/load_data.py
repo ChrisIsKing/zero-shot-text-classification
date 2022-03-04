f2ef27e483f978e89c34bf76e88574c23cc40d7d
ChrisIsKing <christopher.clarke@v75inc.com>
2022-03-03 23:51:53 -0500

    Fixed multi-label bug, sped up vect sampling & re-org pipeline args

--- dataset/load_data.py
+++ dataset/load_data.py
@@ -5,6 +5,9 @@ import spacy
 import gzip
 import csv
 import os
+import time
+from collections import Counter
+from tqdm import tqdm
 from numpy import argmax, argmin
 from os import listdir
 from os.path import isfile, join, basename
@@ -15,6 +18,7 @@ from sentence_transformers import LoggingHandler, util
 g_drive_url = 'https://drive.google.com/uc?id=1qISYYoQNGXtmGWrCsKoK-fBKt8MHXqR7'
 data_path = './dataset'
 nlp = spacy.load("en_core_web_md")
+nlp.disable_pipes(['tagger', 'parser', 'attribute_ruler', 'lemmatizer', 'ner'])
 
 def get_all_zero_data():
     if len(listdir(data_path)) <= 9:
@@ -54,24 +58,37 @@ def get_nli_data():
                 dev_samples.append(InputExample(texts=[row['sentence1'], row['sentence2']], label=label_id))
     return train_samples, dev_samples
 
-def binary_cls_format(arr, sampling='rand', train=True):
+def binary_cls_format(arr, name=None, sampling='rand', train=True):
     examples = []
     if train:
-        label_list = list(set([example[1] for example in arr]))
-        for element in arr:
+        label_list = list(dict.fromkeys([example[1] for example in arr]))
+        label_vectors = {label: nlp(label) for label in label_list}
+        example_list = [x[0] for x in arr]
+
+        if sampling == 'vect':
+            start = time.time()
+            vects = list(nlp.pipe(example_list, n_process=4, batch_size=128))
+            print('Time Elapsed {} ms'.format((time.time() - start)*1000))
+        
+        # count instances
+        count = Counter(example_list)
+        print('Generating {} examples'.format(name))
+        for i, element in enumerate(tqdm(arr)):
             true_label = element[1]
             other_labels = [label for label in label_list if label != element[1]]
+            
             # Generate label for true example
             examples.append(InputExample(texts=[true_label, element[0]], label=1))
 
             # Generate sample based on sampling strategy
-            if sampling == 'rand':
+            if sampling == 'rand' and count[element[0]] < 2:
+                random.seed(i)
                 random_label = random.sample(other_labels, k=2)
                 examples.append(InputExample(texts=[random_label[0], element[0]], label=0))
                 examples.append(InputExample(texts=[random_label[1], element[0]], label=0))
-            elif sampling == 'vect':
-                text_vector = nlp(element[0])
-                other_label_vectors = [nlp(label) for label in other_labels]
+            elif sampling == 'vect' and count[element[0]] < 2:
+                text_vector = vects[i]
+                other_label_vectors = [label_vectors[label] for label in other_labels]
                 scores = [text_vector.similarity(vector) for vector in other_label_vectors]
                 examples.append(InputExample(texts=[other_labels[argmax(scores)], element[0]], label=0))
                 examples.append(InputExample(texts=[other_labels[argmin(scores)], element[0]], label=0))
--- zeroshot_encoder/baseline/binary_bert.py
+++ zeroshot_encoder/baseline/binary_bert.py
@@ -5,6 +5,9 @@ import random
 import seaborn as sns
 import pandas as pd
 from argparse import ArgumentParser
+from pathlib import Path
+from os.path import join
+from collections import Counter
 from tqdm import tqdm
 from sklearn.metrics import classification_report
 from torch.utils.data import DataLoader
@@ -15,11 +18,23 @@ from dataset.load_data import get_all_zero_data, binary_cls_format
 random.seed(42)
 
 def parse_args():
-    args =  ArgumentParser()
+    parser =  ArgumentParser()
+    subparser = parser.add_subparsers(dest='command')
+    train = subparser.add_parser('train')
+    test = subparser.add_parser('test')
 
-    args.add_argument('--train', action='store_true', help='Train model')
-    args.add_argument('--test', action='store_true', help='Evaluate mode')
-    return args.parse_args()
+    # set train arguments
+    train.add_argument('--output', type=str, required=True)
+    train.add_argument('--sampling', type=str, choices=['rand', 'vect'], required=True)
+    train.add_argument('--batch_size', type=int, default=16)
+    train.add_argument('--epochs', type=int, default=3)
+    
+
+    # set test arguments
+    test.add_argument('--model_path', type=str, required=True)
+    test.add_argument('--domain', type=str, choices=['in', 'out'] ,required=True)
+    
+    return parser.parse_args()
 
 logger = logging.getLogger(__name__)
 
@@ -27,19 +42,19 @@ data = get_all_zero_data()
 
 if __name__ == "__main__":
     args = parse_args()
-    if args.train:
+    if args.command == 'train':
         # get keys from data dict
         datasets = list(data.keys())
         datasets.remove("all")
         train = []
         test = []
         for dataset in datasets:
-            train += binary_cls_format(data[dataset]["train"])
+            train += binary_cls_format(data[dataset]["train"], name=dataset, sampling=args.sampling)
             test += binary_cls_format(data[dataset]["test"], train=False)
 
-        train_batch_size = 16
-        num_epochs = 3
-        model_save_path = 'models/binary_bert_rand'
+        train_batch_size = args.batch_size
+        num_epochs = args.epochs
+        model_save_path = join(args.output, args.sampling)
 
         model = CrossEncoder('bert-base-uncased', num_labels=2)
 
@@ -58,41 +73,73 @@ if __name__ == "__main__":
                 evaluation_steps=100000,
                 warmup_steps=warmup_steps,
                 output_path=model_save_path)
-    if args.test:
-        # get keys from data dict
-        datasets = list(data.keys())
-        datasets.remove("all")
+    if args.command == 'test':
+        pred_path = join(args.model_path, 'preds')
+        result_path = join(args.model_path, 'results')
+        Path(pred_path).mkdir(parents=True, exist_ok=True)
+        Path(result_path).mkdir(parents=True, exist_ok=True)
+        if args.domain == 'in':
+            # get keys from data dict
+            datasets = list(data.keys())
+            datasets.remove("all")
 
-        # load model
-        model = CrossEncoder('./models/binary_bert_rand')
+            # load model
+            model = CrossEncoder(args.model_path)
 
-        label_map = ["false", "true"]
+            label_map = ["false", "true"]
 
-        # loop through all datasets
-        for dataset in datasets:
-            test = data[dataset]["test"]
-            labels = list(set(x[1] for x in test))
-            gold = [x[1] for x in test]
+            # loop through all datasets
+            for dataset in datasets:
+                test = data[dataset]["test"]
+                count = Counter([x[0] for x in test])
+                gold = []
+                examples = []
+                labels = list(dict.fromkeys(x[1] for x in test))
 
-            preds = []
-            correct = 0
-            # loop through each test example
-            print("Evaluating dataset: {}".format(dataset))
-            for index, example in enumerate(tqdm(test)):
-                query = [(label, example[0]) for label in labels]
-                results = model.predict(query, apply_softmax=True)
+                # Deal w/multi-label & duplicates
+                duplicates = [k for k,v in count.items() if v > 1]
+                for duplicate in duplicates:
+                    examples.append(duplicate)
+                    
+                    # get labels for duplicate
+                    dup_labels = [x[1] for x in test if x[0] == duplicate]
+                    gold.append([1 if label in dup_labels else 0 for label in labels])
 
-                # compute which pred is higher
-                pred = labels[results[:,1].argmax()]
-                preds.append(pred)
-                if pred == gold[index]:
-                    correct += 1
-            
-            print('{} Dataset Accuracy = {}'.format(dataset, correct/len(test)))
-            report = classification_report(gold, preds, output_dict=True)
-            json.dump([ [test[i][0], pred, gold[i]] for i, pred in enumerate(preds)], open('preds/binary_bert_{}.json'.format(dataset), 'w'), indent=4)
-            # plt = sns.heatmap(pd.DataFrame(report).iloc[:-1, :].T, annot=True)
-            # plt.figure.savefig('figures/binary_bert_{}.png'.format(dataset))
-            df = pd.DataFrame(report).transpose()
-            df.to_csv('./results/binary_bert_{}.csv'.format(dataset))
+                for x, y in test:
+                    if count[x] > 1:
+                        continue
+                    else:
+                        examples.append(x)
+                        gold.append([1 if y==label else 0 for label in labels])
+                
+                assert len(examples) == len(gold)
+                
+                preds = []
+                gold_labels = []
+                correct = 0
+                # loop through each test example
+                print("Evaluating dataset: {}".format(dataset))
+                for index, example in enumerate(tqdm(examples)):
+                    query = [(label, example) for label in labels]
+                    results = model.predict(query, apply_softmax=True)
+
+                    # compute which pred is higher
+                    pred = labels[results[:,1].argmax()]
+                    preds.append(pred)
+                    # load gold labels
+                    g_label = [labels[i] for i, l in enumerate(gold[index]) if l==1]
+                    if pred in g_label:
+                        correct += 1
+                        gold_labels.append(pred)
+                    else:
+                        gold_labels.append(g_label[0])
+                    
+                
+                print('{} Dataset Accuracy = {}'.format(dataset, correct/len(examples)))
+                report = classification_report(gold_labels, preds, output_dict=True)
+                json.dump([ [examples[i], pred, gold_labels[i]] for i, pred in enumerate(preds)], open('{}/{}.json'.format(pred_path,dataset), 'w'), indent=4)
+                # plt = sns.heatmap(pd.DataFrame(report).iloc[:-1, :].T, annot=True)
+                # plt.figure.savefig('figures/binary_bert_{}.png'.format(dataset))
+                df = pd.DataFrame(report).transpose()
+                df.to_csv('{}/{}.csv'.format(result_path, dataset))
             
