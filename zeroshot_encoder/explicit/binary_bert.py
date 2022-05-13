e92a7d97570b88f0013d658316f814310fc26346
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2022-05-13 18:05:44 -0400

    Update: explicit batched eval

--- zeroshot_encoder/explicit/binary_bert.py
+++ zeroshot_encoder/explicit/binary_bert.py
@@ -1,9 +1,9 @@
 import os
 import math
 import logging
+import datetime
 from os.path import join
 from os.path import join as os_join
-from pathlib import Path
 from typing import List, Type, Dict, Optional, Union
 from argparse import ArgumentParser
 
@@ -240,6 +240,7 @@ class ExplicitCrossEncoder:
         inp_dataloader = DataLoader(sentences, batch_size=batch_size, collate_fn=self.smart_batching_collate_text_only, shuffle=False)
 
         show_progress_bar = (logger.getEffectiveLevel() == logging.INFO or logger.getEffectiveLevel() == logging.DEBUG)
+        show_progress_bar = False
 
         iterator = inp_dataloader
         
@@ -259,7 +260,8 @@ class ExplicitCrossEncoder:
                     logits = torch.nn.functional.softmax(logits, dim=1)
                 pred_scores.extend(logits)
 
-        pred_scores = np.asarray([score.cpu().detach().numpy() for score in pred_scores])
+        # pred_scores = np.asarray([score.cpu().detach().numpy() for score in pred_scores])
+        pred_scores = torch.stack(pred_scores, dim=0).detach().cpu()
 
         return pred_scores
 
@@ -301,9 +303,10 @@ def parse_args():
                         help="The initial learning rate for Adam.")
 
     # set test arguments
-    parser_test.add_argument('--model_path', type=str, required=True)
     parser_test.add_argument('--domain', type=str, choices=['in', 'out'], required=True)
     parser_test.add_argument('--mode', type=str, choices=modes, default='vanilla')
+    parser_test.add_argument('--batch_size', type=int, default=32)
+    parser_test.add_argument('--model_path', type=str, required=True)
     
     return parser.parse_args()
 
@@ -354,34 +357,37 @@ if __name__ == "__main__":
             output_path=save_path
         )
     if args.command == 'test':
-        mode = args.mode
-        split = 'test'
-        # split = 'train'  # todo: debugging
-        pred_path = join(args.model_path, 'preds/{}/'.format(args.domain))
-        result_path = join(args.model_path, 'results/{}/'.format(args.domain))
-        if split == 'train':
-            result_path = join(args.model_path, 'results/{}, train/'.format(args.domain))
-        Path(pred_path).mkdir(parents=True, exist_ok=True)
-        Path(result_path).mkdir(parents=True, exist_ok=True)
-        if args.domain == 'in':
-            data = get_data(in_domain_data_path)
-        else:  # out
-            data = get_data(out_of_domain_data_path)
-        # get keys from data dict
-        datasets = list(data.keys())
+        mode, domain, model_path, bsz = args.mode, args.domain, args.model_path, args.batch_size
+        domain_str = 'in-domain' if domain == 'in' else 'out-of-domain'
+        date = datetime.datetime.now().strftime('%m.%d.%Y')
+        date = date[:-4] + date[-2:]  # 2-digit year
+        out_path = join(model_path, 'eval', f'{domain_str}, {date}')
+        os.makedirs(out_path, exist_ok=True)
 
-        model = ExplicitCrossEncoder.from_pretrained(args.model_path)
+        data = get_data(in_domain_data_path if domain == 'in' else out_of_domain_data_path)
+        model = ExplicitCrossEncoder.from_pretrained(model_path)  # load model
+        sep_token = sconfig('training.implicit-on-text.encode-sep.aspect-sep-token')
+        aspect2aspect_token = sconfig('training.implicit-on-text.encode-aspect.aspect2aspect-token')
 
-        label_map = ["false", "true"]
+        logger = get_logger('Binary Bert Eval')
+        d_log = dict(mode=mode, domain=domain, batch_size=bsz, path=model_path)
+        logger.info(f'Evaluating Binary Bert with {log_dict(d_log)} and saving to {logi(out_path)}... ')
 
-        # loop through all datasets
-        for dataset in datasets:
-            examples = data[dataset][split]
-            label_options = data[dataset]['labels']
-            aspect = data[dataset]['aspect']
-            preds = []
-            gold = []
-            correct = 0
+        eval_loss: Dict[str, np.array] = dict()  # a sense of how badly the model makes the prediction
+        dataset_names = [dnm for dnm, d_dset in sconfig('UTCD.datasets').items() if d_dset['domain'] == domain]
+
+        for dnm in dataset_names:  # loop through all datasets
+            dset = data[dnm]
+            split = 'test'
+            txts, aspect = dset[split], dset['aspect']
+            d_dset = sconfig(f'UTCD.datasets.{dnm}.splits.{split}')
+            label_options, multi_label = d_dset['labels'], d_dset['multi_label']
+            n_options = len(label_options)
+            label2id = {lbl: i for i, lbl in enumerate(label_options)}
+            n_txt = sconfig(f'UTCD.datasets.{dnm}.splits.{split}.n_text')
+            d_log = {'#text': n_txt, '#label': n_options}
+            logger.info(f'Evaluating {logi(dnm)} with {log_dict(d_log)}...')
+            arr_preds, arr_labels = np.empty(n_txt, dtype=int), np.empty(n_txt, dtype=int)
 
             txt_n_lbs2query = None
             if mode in ['vanilla', 'explicit']:
@@ -391,39 +397,105 @@ if __name__ == "__main__":
                 def txt_n_lbs2query(txt: str, lbs: List[str]) -> List[List[str]]:
                     return [[txt, f'{lb} {aspect}'] for lb in lbs]
             elif mode == 'implicit-on-text-encode-aspect':
-                aspect_token = sconfig('training.implicit-on-text.encode-aspect.aspect2aspect-token')[aspect]
-
                 def txt_n_lbs2query(txt: str, lbs: List[str]) -> List[List[str]]:
-                    return [[f'{aspect_token} {txt}', lb] for lb in lbs]
+                    return [[f'{aspect2aspect_token[aspect]} {txt}', lb] for lb in lbs]
             elif mode == 'implicit-on-text-encode-sep':
-                sep_token = sconfig('training.implicit-on-text.encode-sep.aspect-sep-token')
-
                 def txt_n_lbs2query(txt: str, lbs: List[str]) -> List[List[str]]:
                     return [[f'{aspect} {sep_token} {txt}', lb] for lb in lbs]
 
+            gen = group_n(txts.items(), n=bsz)
             # loop through each test example
-            print(f'Evaluating dataset: {logi(dataset)}')
-            for index, (txt_, gold_labels) in enumerate(tqdm(examples.items())):
-                query = txt_n_lbs2query(txt_, label_options)
-                results = model.predict(query)
+            for i_grp, group in enumerate(tqdm(gen, desc=dnm, unit='group', total=math.ceil(n_txt/bsz))):
+                txts_, lst_labels = zip(*group)
+                lst_labels: List[List[int]] = [[label2id[lb] for lb in labels] for labels in lst_labels]
+                query = sum([txt_n_lbs2query(t, label_options) for t in txts_], start=[])  # (n_options x bsz, 2)
+                # probability for positive class
+                logits = model.predict(query, batch_size=bsz)[:, 1]
+                logits = logits.reshape(-1, n_options)
+                preds = logits.argmax(axis=1)
+                trues = torch.empty_like(preds)
+                for i, pred, labels in zip(range(bsz), preds, lst_labels):
+                    # if false prediction, pick one of the correct labels arbitrarily
+                    trues[i] = pred if pred in labels else labels[0]
+                idx_strt = i_grp*bsz
+                arr_preds[idx_strt:idx_strt+bsz], arr_labels[idx_strt:idx_strt+bsz] = preds.cpu(), trues.cpu()
 
-                # compute which pred is higher
-                pred = label_options[results[:, 1].argmax()]
-                preds.append(pred)
-
-                # ic(txt_, gold_labels)
-                # ic(results[:, 1], label_options)
-                #
-                # if index >= 10:
-                #     exit(1)
-
-                if pred in gold_labels:
-                    correct += 1
-                    gold.append(pred)
-                else:
-                    gold.append(gold_labels[0])
-            
-            print(f'{logi(dataset)} Dataset Accuracy: {logi(correct/len(examples))}')
-            report = classification_report(gold, preds, output_dict=True, zero_division=0)
+            args = dict(zero_division=0, target_names=label_options, output_dict=True)  # disables warning
+            report = classification_report(arr_labels, arr_preds, **args)
+            acc = f'{report["accuracy"]:.3f}'
+            logger.info(f'{logi(dnm)} Classification Accuracy: {logi(acc)}')
             df = pd.DataFrame(report).transpose()
-            df.to_csv('{}/{}.csv'.format(result_path, dataset))
+            df.to_csv(join(out_path, f'{dnm}.csv'))
+        # mode = args.mode
+        # split = 'test'
+        # # split = 'train'  # todo: debugging
+        # pred_path = join(args.model_path, 'preds/{}/'.format(args.domain))
+        # result_path = join(args.model_path, 'results/{}/'.format(args.domain))
+        # if split == 'train':
+        #     result_path = join(args.model_path, 'results/{}, train/'.format(args.domain))
+        # Path(pred_path).mkdir(parents=True, exist_ok=True)
+        # Path(result_path).mkdir(parents=True, exist_ok=True)
+        # if args.domain == 'in':
+        #     data = get_data(in_domain_data_path)
+        # else:  # out
+        #     data = get_data(out_of_domain_data_path)
+        # # get keys from data dict
+        # datasets = list(data.keys())
+        #
+        # model = ExplicitCrossEncoder.from_pretrained(args.model_path)
+        #
+        # label_map = ["false", "true"]
+        #
+        # # loop through all datasets
+        # for dataset in datasets:
+        #     examples = data[dataset][split]
+        #     label_options = data[dataset]['labels']
+        #     aspect = data[dataset]['aspect']
+        #     preds = []
+        #     gold = []
+        #     correct = 0
+        #
+        #     txt_n_lbs2query = None
+        #     if mode in ['vanilla', 'explicit']:
+        #         def txt_n_lbs2query(txt: str, lbs: List[str]) -> List[List[str]]:
+        #             return [[txt, lb] for lb in lbs]
+        #     elif mode == 'implicit':
+        #         def txt_n_lbs2query(txt: str, lbs: List[str]) -> List[List[str]]:
+        #             return [[txt, f'{lb} {aspect}'] for lb in lbs]
+        #     elif mode == 'implicit-on-text-encode-aspect':
+        #         aspect_token = sconfig('training.implicit-on-text.encode-aspect.aspect2aspect-token')[aspect]
+        #
+        #         def txt_n_lbs2query(txt: str, lbs: List[str]) -> List[List[str]]:
+        #             return [[f'{aspect_token} {txt}', lb] for lb in lbs]
+        #     elif mode == 'implicit-on-text-encode-sep':
+        #         sep_token = sconfig('training.implicit-on-text.encode-sep.aspect-sep-token')
+        #
+        #         def txt_n_lbs2query(txt: str, lbs: List[str]) -> List[List[str]]:
+        #             return [[f'{aspect} {sep_token} {txt}', lb] for lb in lbs]
+        #
+        #     # loop through each test example
+        #     print(f'Evaluating dataset: {logi(dataset)}')
+        #     for index, (txt_, gold_labels) in enumerate(tqdm(examples.items())):
+        #         query = txt_n_lbs2query(txt_, label_options)
+        #         results = model.predict(query)
+        #
+        #         # compute which pred is higher
+        #         pred = label_options[results[:, 1].argmax()]
+        #         preds.append(pred)
+        #
+        #         # ic(txt_, gold_labels)
+        #         # ic(results[:, 1], label_options)
+        #         #
+        #         # if index >= 10:
+        #         #     exit(1)
+        #
+        #         if pred in gold_labels:
+        #             correct += 1
+        #             gold.append(pred)
+        #         else:
+        #             gold.append(gold_labels[0])
+        #
+        #     print(f'{logi(dataset)} Dataset Accuracy: {logi(correct/len(examples))}')
+        #     report = classification_report(gold, preds, output_dict=True, zero_division=0)
+        #     df = pd.DataFrame(report).transpose()
+        #     df.to_csv('{}/{}.csv'.format(result_path, dataset))
