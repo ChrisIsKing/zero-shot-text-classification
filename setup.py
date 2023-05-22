3ea41770d3d5b02d616a3c45fb087befcf7da022
Stefan Heng <stefan.hg@outlook.com>
2023-05-22 11:25:46 -0400

    Chore: publish package; Fix: hf hub required version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.2'
+VERSION = '0.2.1'
 DESCRIPTION = """
 code and data for the Findings of ACL'23 paper Label Agnostic Pre-training for Zero-shot Text Classification 
 by Christopher Clarke, Yuzhao Heng, Yiping Kang, Krisztian Flautner, Lingjia Tang and Jason Mars
@@ -15,14 +15,14 @@ setup(
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     url='https://github.com/ChrisIsKing/zero-shot-text-classification',
-    download_url='https://github.com/ChrisIsKing/zero-shot-text-classification/archive/refs/tags/v0.1.2.tar.gz',
+    download_url='https://github.com/ChrisIsKing/zero-shot-text-classification/archive/refs/tags/v0.2.1.tar.gz',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'gdown==4.5.4', 'openai==0.25.0', 'requests==2.28.1', 'tenacity==8.1.0',
         'spacy==3.2.2', 'nltk==3.7', 'scikit-learn==1.1.3',
         'torch==1.12.0', 'sentence-transformers==2.2.0', 'transformers==4.16.2',
-        'datasets==1.18.3',
+        'datasets==1.18.3', 'huggingface-hub==0.4.0',
         'stefutils==0.22.2'
     ],
     keywords=['python', 'nlp', 'machine-learning', 'deep-learning', 'text-classification', 'zero-shot-classification'],
